9f16e9d7689391d0a69859f2e5d05d2403247f60
tomoya0605 <t.tomoya0605@gmail.com>
2021-01-01 15:19:31 +0000

    変更点

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -24,13 +24,11 @@
       </aside>
     </div>
     <div class="text-center mb10">
-      @if (User::id() == $review['id'])
       <form action="/{{ $review->id }}" method="POST">
         @method('DELETE')
         @csrf
         <button class="btn btn-info btn-danger" type="submit" onclick="return confirm('削除します。よろしいですか？')">削除</button>
       </form>
-      @endif
     </div>
       <a href="{{ route('index') }}" class='btn btn-info btn-back mb20'>一覧へ戻る</a>
   </div>
